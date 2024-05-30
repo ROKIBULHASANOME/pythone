e2e77afae230a77ac809414e8816e0014b2d6874
ROKIBULHASANOME <rokibulhasanome@gmail.com>
2024-05-30 14:01:52 +0600

    first commit

---
+++ d.py
@@ -0,0 +1,20 @@
+from new_func import new_func
+
+
+nterms =int(input("How many terms?"))
+
+n1,n2=0,1
+count=0
+if nterms<=0
+print("Please enter a positive integer")
+new_func() nterms==1
+print("Fibonacci sequeence up to:", nterms,":")
+print(n1)
+else
+print("Fibonacci sequence:")
+while count< nterms:
+    print(n1)
+    nth=n1+n2
+    n1=n2
+    n2=nth
+    count+=1
